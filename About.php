c06e335f80fa41eb19ca0498d988f3033b9e7c34
maxlayn1 <maxlayn1@gmail.com>
2024-10-17 10:26:00 -0500

    finalized php conversion (ie issues with links fixed)

--- About.php
+++ About.php
@@ -9,14 +9,14 @@
 </head>
 <body>
     <div class="w3-bar topMenu w3-white">
-        <a href="testIndex.html">
+        <a href="index.php">
             <img src="imgs\Pharmacy_Green_Cross.svg.png" alt="website logo linking to index page" 
                 class="logo w3-image w3-bar-item" style="width: 150px; height: auto;">
         </a>
-        <a href="About.html" class="rightLink w3-bar-item w3-button w3-right w3-hover-text-green w3-hover-white">About</a>
-        <a href="Supplements.html" class="rightLink w3-bar-item w3-button w3-right w3-hover-text-green w3-hover-white">Supplements</a>
-        <a href="Calculator.html" class="rightLink w3-bar-item w3-button w3-right w3-hover-text-green w3-hover-white" >Calculator</a>
-        <a href="Dieting.html" class="rightLink w3-bar-item w3-button w3-right w3-hover-text-green w3-hover-white">Dieting</a> 
+        <a href="About.php" class="rightLink w3-bar-item w3-button w3-right w3-hover-text-green w3-hover-white">About</a>
+        <a href="Supplements.php" class="rightLink w3-bar-item w3-button w3-right w3-hover-text-green w3-hover-white">Supplements</a>
+        <a href="Calculator.php" class="rightLink w3-bar-item w3-button w3-right w3-hover-text-green w3-hover-white" >Calculator</a>
+        <a href="Dieting.php" class="rightLink w3-bar-item w3-button w3-right w3-hover-text-green w3-hover-white">Dieting</a> 
     </div>
 </body>
 </html>
--- index.php
+++ index.php
@@ -6,19 +6,19 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" type="text/css" href="styles.css" />
     <link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3.css">
-    <title>Supplement Website for Final Project</title>
+    <title>Home Page</title>
 </head>
 
 <body>
     <div class="w3-bar topMenu w3-white">
-        <a href="testIndex.html">
+        <a href="index.php">
             <img src="imgs\Pharmacy_Green_Cross.svg.png" alt="website logo linking to index page" 
-                class="logo w3-image w3-bar-item" style="width: 150px; height: auto;">css
+                class="logo w3-image w3-bar-item" style="width: 150px; height: auto;">
         </a>
-        <a href="About.html" class="rightLink w3-bar-item w3-button w3-right w3-hover-text-green w3-hover-white">About</a>
-        <a href="Supplements.html" class="rightLink w3-bar-item w3-button w3-right w3-hover-text-green w3-hover-white">Supplements</a>
-        <a href="Calculator.html" class="rightLink w3-bar-item w3-button w3-right w3-hover-text-green w3-hover-white" >Calculator</a>
-        <a href="Dieting.html" class="rightLink w3-bar-item w3-button w3-right w3-hover-text-green w3-hover-white">Dieting</a> 
+        <a href="About.php" class="rightLink w3-bar-item w3-button w3-right w3-hover-text-green w3-hover-white">About</a>
+        <a href="Supplements.php" class="rightLink w3-bar-item w3-button w3-right w3-hover-text-green w3-hover-white">Supplements</a>
+        <a href="Calculator.php" class="rightLink w3-bar-item w3-button w3-right w3-hover-text-green w3-hover-white" >Calculator</a>
+        <a href="Dieting.php" class="rightLink w3-bar-item w3-button w3-right w3-hover-text-green w3-hover-white">Dieting</a> 
     </div>
     
     <div class="centerPara w3-container w3-center">
